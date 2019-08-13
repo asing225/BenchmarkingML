c8b53c3601e309630bced4158a6723b4acf9022f
Sakshi7373 <sakshi7373@gmail.com>
2019-08-12 22:00:48 -0700

    Gpu part added

--- app/src/main/java/com/example/benchmarkingml_team4/MainActivity.java
+++ app/src/main/java/com/example/benchmarkingml_team4/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     EditText kvalue;
     SeekBar seek;
     Button classify;
+    Button GPU;
     int trainSize = 0, testSize = 0, splitRatio = 1, k = 2;
     String writedata = "";
 
@@ -46,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         kvalue = (EditText) findViewById(R.id.k_value);
 
         classify = (Button) findViewById(R.id.classify);
+        GPU= (Button)findViewById(R.id.gpuButton);
         seek = (SeekBar) findViewById(R.id.seekBar);
         seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int newVal = 0;
@@ -73,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Intent intent = new Intent(MainActivity.this, ResultPage.class);
+        Intent intent = new Intent(MainActivity.this, ResultPage.class);
         classify.setOnClickListener(new View.OnClickListener() {
             Context context = MainActivity.this;
             @Override
@@ -193,16 +195,65 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
 
-    private void writeToFile(String data, Context context) {
-        try {
-            outputStreamWriter = new OutputStreamWriter(context.openFileOutput("logfile.txt", Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
-            outputStreamWriter.close();
-        }
-        catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
-    }
-}
+    GPU.setOnClickListener(new View.OnClickListener() {
+        Context context = MainActivity.this;
+        @Override
+        public void onClick(View v) {
+
+            if (splitRatio == 0 || splitRatio == 100) {
+                Toast.makeText(MainActivity.this, "Please choose value between 1 and 100", Toast.LENGTH_LONG).show();
+            }
+            else {
+                ReadDataSet data = new ReadDataSet();
+                BufferedReader reader = data.readDataFile(context, R.raw.breastcancer);
+                Instances instance = null;
+                try {
+                    instance = new Instances(reader);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                int algorithmCount = 0;
+                Toast.makeText(MainActivity.this, "Please wait for results.", Toast.LENGTH_SHORT).show();
+                if (rf.isChecked()) {
+                    algorithmCount++;
+                    Randomforest rf = new Randomforest();
+                    try{
+                        writedata = new String();
+                        rf.process_RF(instance,trainSize,testSize);
+                        intent.putExtra("result", rf.getAlgoSummary());
+                        writedata = writedata + "Random Forest \n" + rf.getAlgoSummary() + "\r\n " ;
+                        writedata = writedata + "\n True Positive Rate: \t" + rf.getTruePositiveRate();
+                        writedata = writedata + "\n True Negative Rate: \t" + rf.getTrueNegativeRate();
+                        writedata = writedata + "\n False Negative Rate: \t" + rf.getFalseNegativeRate();
+                        writedata = writedata + "\n False Positive Rate: \t" + rf.getFalsePositiveRate();
+                        writedata = writedata + "\n Hter: \t" + rf.getHter();
+                        writedata = writedata + "\n Train Time: \t" + rf.getTrainTime();
+                        writedata = writedata + "\n Test Time: \t" + rf.getTestTime();
+                        writedata = writedata + "\n Total Execution Time: \t" + rf.getExecution_Time();
+                        writeToFile(writedata, MainActivity.this);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    if (algorithmCount == 0) {
+                        Toast.makeText(MainActivity.this, "Please select an algorithm.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Intent intent = new Intent(MainActivity.this, ResultPage.class);
+                        startActivity(intent);
+                    }
+
+                }
+                }}});
+            }
+                private void writeToFile (String data, Context context){
+                    try {
+                        outputStreamWriter = new OutputStreamWriter(context.openFileOutput("logfile.txt", Context.MODE_PRIVATE));
+                        outputStreamWriter.write(data);
+                        outputStreamWriter.close();
+                    } catch (IOException e) {
+                        Log.e("Exception", "File write failed: " + e.toString());
+                    }
+                }
+            }
